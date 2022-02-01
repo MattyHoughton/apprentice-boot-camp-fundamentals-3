b6a7431aa5935f4415afc81fbe62c0467f7e6533
MattyHoughton <mattyh1997@hotmail.com>
2022-02-01 12:30:36 +0000

    fixed

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -3,31 +3,31 @@ package tax;
 public class DefaultTaxCalculator extends TaxCalculator {
     public int calculateTax(Vehicle vehicle) {
         if(vehicle.getFuelType() == FuelType.PETROL) {
-            if (vehicle.getCo2Emissions() == 0) {
+            if (vehicle.getCo2Emissions() <= 0) {
                 return 0;
-            } else if (vehicle.getCo2Emissions() == 50) {
+            } else if (vehicle.getCo2Emissions() <= 50) {
                 return 10;
-            } else if (vehicle.getCo2Emissions() == 75) {
+            } else if (vehicle.getCo2Emissions() <= 75) {
                 return 25;
-            } else if (vehicle.getCo2Emissions() == 90) {
+            } else if (vehicle.getCo2Emissions() <= 90) {
                 return 105;
-            } else if (vehicle.getCo2Emissions() == 100) {
+            } else if (vehicle.getCo2Emissions() <= 100) {
                 return 125;
-            } else if (vehicle.getCo2Emissions() == 110) {
+            } else if (vehicle.getCo2Emissions() <= 110) {
                 return 145;
-            } else if (vehicle.getCo2Emissions() == 130) {
+            } else if (vehicle.getCo2Emissions() <= 130) {
                 return 165;
-            } else if (vehicle.getCo2Emissions() == 150) {
+            } else if (vehicle.getCo2Emissions() <= 150) {
                 return 205;
-            } else if (vehicle.getCo2Emissions() == 170) {
+            } else if (vehicle.getCo2Emissions() <= 170) {
                 return 515;
-            } else if (vehicle.getCo2Emissions() == 190) {
+            } else if (vehicle.getCo2Emissions() <= 190) {
                 return 830;
-            } else if (vehicle.getCo2Emissions() == 225) {
+            } else if (vehicle.getCo2Emissions() <= 225) {
                 return 1240;
-            } else if (vehicle.getCo2Emissions() == 255) {
+            } else if (vehicle.getCo2Emissions() <= 255) {
                 return 1760;
-            } else if (vehicle.getCo2Emissions() == 256) {
+            } else if (vehicle.getCo2Emissions() <= 256) {
                 return 2070;
             }
         }
@@ -57,6 +57,34 @@ public class DefaultTaxCalculator extends TaxCalculator {
                 return 1750;
             } else {
                 return 2060;
+            }
+        } else if (vehicle.getFuelType() == FuelType.DIESEL) {
+            if (vehicle.getCo2Emissions() <= 0) {
+                return 0;
+            } else if (vehicle.getCo2Emissions() <= 50) {
+                return 25;
+            } else if (vehicle.getCo2Emissions() <= 75) {
+                return 105;
+            } else if (vehicle.getCo2Emissions() <= 90) {
+                return 125;
+            } else if (vehicle.getCo2Emissions() <= 100) {
+                return 145;
+            } else if (vehicle.getCo2Emissions() <= 110) {
+                return 165;
+            } else if (vehicle.getCo2Emissions() <= 130) {
+                return 205;
+            } else if (vehicle.getCo2Emissions() <= 150) {
+                return 515;
+            } else if (vehicle.getCo2Emissions() <= 170) {
+                return 830;
+            } else if (vehicle.getCo2Emissions() <= 190) {
+                return 1240;
+            } else if (vehicle.getCo2Emissions() <= 225) {
+                return 1760;
+            } else if (vehicle.getCo2Emissions() <= 255) {
+                return 2070;
+            } else if (vehicle.getCo2Emissions() <= 256) {
+                return 2070;
             }
         }
         return -1;
