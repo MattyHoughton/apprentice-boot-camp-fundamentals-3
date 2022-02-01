96498b7d057a93e9d0b654a74f462a62e620c5d0
MattyHoughton <mattyh1997@hotmail.com>
2022-02-01 14:25:42 +0000

    adding first year

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -15,90 +15,99 @@ public class DefaultTaxCalculator extends TaxCalculator {
     }
 
     public int calculateTax(Vehicle vehicle) {
-
-        if(vehicle.getFuelType() == FuelType.PETROL) {
-            if (vehicle.getCo2Emissions() <= 0) {
-                return 0;
-            } else if (vehicle.getCo2Emissions() <= 50) {
-                return 10;
-            } else if (vehicle.getCo2Emissions() <= 75) {
-                return 25;
-            } else if (vehicle.getCo2Emissions() <= 90) {
-                return 105;
-            } else if (vehicle.getCo2Emissions() <= 100) {
-                return 125;
-            } else if (vehicle.getCo2Emissions() <= 110) {
-                return 145;
-            } else if (vehicle.getCo2Emissions() <= 130) {
-                return 165;
-            } else if (vehicle.getCo2Emissions() <= 150) {
-                return 205;
-            } else if (vehicle.getCo2Emissions() <= 170) {
-                return 515;
-            } else if (vehicle.getCo2Emissions() <= 190) {
-                return 830;
-            } else if (vehicle.getCo2Emissions() <= 225) {
-                return 1240;
-            } else if (vehicle.getCo2Emissions() <= 255) {
-                return 1760;
-            } else if (vehicle.getCo2Emissions() <= 256) {
-                return 2070;
+        if (!this.firstYear) {
+            if (vehicle.getFuelType() == FuelType.PETROL) {
+                if (vehicle.getCo2Emissions() <= 0) {
+                    return 0;
+                } else if (vehicle.getCo2Emissions() <= 50) {
+                    return 10;
+                } else if (vehicle.getCo2Emissions() <= 75) {
+                    return 25;
+                } else if (vehicle.getCo2Emissions() <= 90) {
+                    return 105;
+                } else if (vehicle.getCo2Emissions() <= 100) {
+                    return 125;
+                } else if (vehicle.getCo2Emissions() <= 110) {
+                    return 145;
+                } else if (vehicle.getCo2Emissions() <= 130) {
+                    return 165;
+                } else if (vehicle.getCo2Emissions() <= 150) {
+                    return 205;
+                } else if (vehicle.getCo2Emissions() <= 170) {
+                    return 515;
+                } else if (vehicle.getCo2Emissions() <= 190) {
+                    return 830;
+                } else if (vehicle.getCo2Emissions() <= 225) {
+                    return 1240;
+                } else if (vehicle.getCo2Emissions() <= 255) {
+                    return 1760;
+                } else if (vehicle.getCo2Emissions() <= 256) {
+                    return 2070;
+                }
             }
-        }
-        if(vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL || vehicle.getFuelType() == FuelType.ELECTRIC){
-            int co2Emissions = vehicle.getCo2Emissions();
-            if(co2Emissions <= 50){
-                return 0;
-            } else if (co2Emissions <= 75 ){
-                return 15;
-            } else if (co2Emissions <= 90 ){
-                return 95;
-            } else if (co2Emissions <= 100){
-                return 115;
-            } else if (co2Emissions <= 110){
-                return 135;
-            } else if (co2Emissions <= 130){
-                return 155;
-            } else if (co2Emissions <= 150){
-                return 195;
-            } else if (co2Emissions <= 170){
-                return 505;
-            } else if (co2Emissions <= 190){
-                return 820;
-            } else if (co2Emissions <= 225){
-                return 1230;
-            } else if (co2Emissions <= 255){
-                return 1750;
-            } else {
-                return 2060;
+            if (vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL || vehicle.getFuelType() == FuelType.ELECTRIC) {
+                int co2Emissions = vehicle.getCo2Emissions();
+                if (co2Emissions <= 50) {
+                    return 0;
+                } else if (co2Emissions <= 75) {
+                    return 15;
+                } else if (co2Emissions <= 90) {
+                    return 95;
+                } else if (co2Emissions <= 100) {
+                    return 115;
+                } else if (co2Emissions <= 110) {
+                    return 135;
+                } else if (co2Emissions <= 130) {
+                    return 155;
+                } else if (co2Emissions <= 150) {
+                    return 195;
+                } else if (co2Emissions <= 170) {
+                    return 505;
+                } else if (co2Emissions <= 190) {
+                    return 820;
+                } else if (co2Emissions <= 225) {
+                    return 1230;
+                } else if (co2Emissions <= 255) {
+                    return 1750;
+                } else {
+                    return 2060;
+                }
+            } else if (vehicle.getFuelType() == FuelType.DIESEL) {
+                if (vehicle.getCo2Emissions() <= 0) {
+                    return 0;
+                } else if (vehicle.getCo2Emissions() <= 50) {
+                    return 25;
+                } else if (vehicle.getCo2Emissions() <= 75) {
+                    return 105;
+                } else if (vehicle.getCo2Emissions() <= 90) {
+                    return 125;
+                } else if (vehicle.getCo2Emissions() <= 100) {
+                    return 145;
+                } else if (vehicle.getCo2Emissions() <= 110) {
+                    return 165;
+                } else if (vehicle.getCo2Emissions() <= 130) {
+                    return 205;
+                } else if (vehicle.getCo2Emissions() <= 150) {
+                    return 515;
+                } else if (vehicle.getCo2Emissions() <= 170) {
+                    return 830;
+                } else if (vehicle.getCo2Emissions() <= 190) {
+                    return 1240;
+                } else if (vehicle.getCo2Emissions() <= 225) {
+                    return 1760;
+                } else if (vehicle.getCo2Emissions() <= 255) {
+                    return 2070;
+                } else if (vehicle.getCo2Emissions() <= 256) {
+                    return 2070;
+                }
             }
-        } else if (vehicle.getFuelType() == FuelType.DIESEL) {
-            if (vehicle.getCo2Emissions() <= 0) {
+        } else if (this.firstYear) {
+            if (vehicle.getFuelType() == FuelType.PETROL) {
+                return 140;
+            } else if (vehicle.getFuelType() == FuelType.ELECTRIC) {
                 return 0;
-            } else if (vehicle.getCo2Emissions() <= 50) {
-                return 25;
-            } else if (vehicle.getCo2Emissions() <= 75) {
-                return 105;
-            } else if (vehicle.getCo2Emissions() <= 90) {
-                return 125;
-            } else if (vehicle.getCo2Emissions() <= 100) {
-                return 145;
-            } else if (vehicle.getCo2Emissions() <= 110) {
-                return 165;
-            } else if (vehicle.getCo2Emissions() <= 130) {
-                return 205;
-            } else if (vehicle.getCo2Emissions() <= 150) {
-                return 515;
-            } else if (vehicle.getCo2Emissions() <= 170) {
-                return 830;
-            } else if (vehicle.getCo2Emissions() <= 190) {
-                return 1240;
-            } else if (vehicle.getCo2Emissions() <= 225) {
-                return 1760;
-            } else if (vehicle.getCo2Emissions() <= 255) {
-                return 2070;
-            } else if (vehicle.getCo2Emissions() <= 256) {
-                return 2070;
+            } else if (vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL) {
+                return 130;
             }
         }
         return -1;
