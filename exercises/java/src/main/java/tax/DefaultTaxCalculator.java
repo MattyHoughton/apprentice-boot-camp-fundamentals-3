25fbc104de4f5763fe37e3d2b121950f8ecbdab4
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2022-02-01 14:29:31 +0000

    Story 3 complete

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -15,8 +15,19 @@ public class DefaultTaxCalculator extends TaxCalculator {
     }
 
     public int calculateTax(Vehicle vehicle) {
-
-        if(vehicle.getFuelType() == FuelType.PETROL) {
+        if (firstYearExpensive) {
+            if (getYear() - vehicle.getDateOfFirstRegistration().getYear() > 1
+                    && vehicle.getListPrice() > 40000) {
+                if (vehicle.getFuelType() == FuelType.ELECTRIC) {
+                    return 310;
+                }
+                if (vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL) {
+                    return 440;
+                }
+                return 450;
+            }
+        }
+        if (vehicle.getFuelType() == FuelType.PETROL) {
             if (vehicle.getCo2Emissions() <= 0) {
                 return 0;
             } else if (vehicle.getCo2Emissions() <= 50) {
@@ -45,29 +56,29 @@ public class DefaultTaxCalculator extends TaxCalculator {
                 return 2070;
             }
         }
-        if(vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL || vehicle.getFuelType() == FuelType.ELECTRIC){
+        if (vehicle.getFuelType() == FuelType.ALTERNATIVE_FUEL || vehicle.getFuelType() == FuelType.ELECTRIC) {
             int co2Emissions = vehicle.getCo2Emissions();
-            if(co2Emissions <= 50){
+            if (co2Emissions <= 50) {
                 return 0;
-            } else if (co2Emissions <= 75 ){
+            } else if (co2Emissions <= 75) {
                 return 15;
-            } else if (co2Emissions <= 90 ){
+            } else if (co2Emissions <= 90) {
                 return 95;
-            } else if (co2Emissions <= 100){
+            } else if (co2Emissions <= 100) {
                 return 115;
-            } else if (co2Emissions <= 110){
+            } else if (co2Emissions <= 110) {
                 return 135;
-            } else if (co2Emissions <= 130){
+            } else if (co2Emissions <= 130) {
                 return 155;
-            } else if (co2Emissions <= 150){
+            } else if (co2Emissions <= 150) {
                 return 195;
-            } else if (co2Emissions <= 170){
+            } else if (co2Emissions <= 170) {
                 return 505;
-            } else if (co2Emissions <= 190){
+            } else if (co2Emissions <= 190) {
                 return 820;
-            } else if (co2Emissions <= 225){
+            } else if (co2Emissions <= 225) {
                 return 1230;
-            } else if (co2Emissions <= 255){
+            } else if (co2Emissions <= 255) {
                 return 1750;
             } else {
                 return 2060;
